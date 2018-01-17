9ebfc8e5df1a3dd616040fc15a04b108a0466c81
marianne.trizha <marianne.escubin@gmail.com>
2018-01-17 20:54:12 +0800

    12 days of Christmas

---
+++ lib/twelve_days.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+$days = array("first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "ninth", "tenth", "eleventh", "twelfth"); 
+
+	$gifts = array(
+		"A partridge in a pair tree,",
+		"Two turtle doves,", 
+		"Three french hens,", 
+		"Four calling birds,", 
+		"Five golden rings,",
+		"Six geese- a layin,",
+		"Seven swans a- swimming,",
+		"Eight maids a-milking,,",
+		"Nine ladies dancing,", 
+		"Ten lords a-leaping,",
+		"Eleven pipers piping,",
+		"Twelve drummers drumming,");
+	
+function getTitle() {
+	return "12 Days of Christmas with Mariah Carey";
+}
+
+function getLyrics($day) {
+	$days = array("first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eight", "ninth", "tenth", "eleventh", "twelfth"); 
+
+	$gifts = array(
+		"A partridge in a pair tree,",
+		"Two turtle doves,", 
+		"Three french hens,", 
+		"Four calling birds,", 
+		"Five golden rings,",
+		"Six geese- a layin,",
+		"Seven swans a- swimming,",
+		"Eight maids a-milking,,",
+		"Nine ladies dancing,", 
+		"Ten lords a-leaping,",
+		"Eleven pipers piping,",
+		"Twelve drummers drumming,");
+
+	 // echo "For the " ."<button> '$days[$day]' </button>" . " of days of Christmas, my true love sent to me... " . "<p id=line>'$gifts[$day]'</p>";
+}
